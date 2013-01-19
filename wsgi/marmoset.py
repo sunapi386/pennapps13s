3bdeeac9ca5ef1f42bbac923575e9022f319ac17
Michael Chang <michaelchang@khanacademy.org>
2013-01-19 14:05:55 -0500

    blah

--- wsgi/marmoset.py
+++ wsgi/marmoset.py
@@ -1,21 +1,21 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.debug = True
 db_uri = (
-    "postgresql+psycopg2://{0}:{1}@{2}:{3}/{4}".format(
-        "admin",
-        "weNIq4pDKBpB",
-        os.getenv("OPENSHIFT_POSTGRESQL_DB_HOST"),
-        os.getenv("OPENSHIFT_POSTGRESQL_DB_PORT"),
-        "marmoset"
+    'postgresql+psycopg2://{0}:{1}@{2}:{3}/{4}'.format(
+        'admin',
+        'weNIq4pDKBpB',
+        os.getenv('OPENSHIFT_POSTGRESQL_DB_HOST'),
+        os.getenv('OPENSHIFT_POSTGRESQL_DB_PORT'),
+        'marmoset'
     )
 )
 
-fb_api_key = "434597593277609"
-fb_app_secret = "91be1f6dc17828d953a8cdff997113af"
+fb_api_key = '434597593277609'
+fb_app_secret = '91be1f6dc17828d953a8cdff997113af'
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
@@ -29,16 +29,23 @@ class User(db.Model):
     self.fbid = fbid
 
   def __repr__(self):
-    return "<User {0} {1}>".format(self.id, self.fbid)
+    return '<User {0} {1}>'.format(self.id, self.fbid)
 
-@app.route("/")
+@app.route('/login')
+    return redirect('')
+
+@app.route('/loginsuccess')
+def loginsuccess():
+    return redirect(url_for('hello'))
+
+@app.route('/')
 def hello():
     #user = User.query.filter_by(fbid='100005073064107').first()
     #user = User(100005073064107)
     #db.session.add(user)
     #db.session.commit()
-    return render_template("index.html")
+    return render_template('index.html')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
 
